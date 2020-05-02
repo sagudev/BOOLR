d719fbffebe1d05e71742f30d90b72bde32be122
sagudev <samo.golez@outlook.com>
2020-05-02 14:22:01 +0200

    test

--- build.sh
+++ build.sh
@@ -2,3 +2,8 @@
 set -ex
 wasm-pack build --target web
 #python3 -m http.server
+# post-bindgen
+#sed -i 's/typeof .* == '\''function'\'' ? .* : notDefined('\''.*'\'');/OK/g' test
+cp ./pkg/BOOLR.js ./pkg/BOOLR.js.bk
+sed -i '/typeof .* == '\''function'\'' ? .* : notDefined('\''.*'\'');/{s! == '\''function'\'' ? ! == '\''function'\'' ? function(){ !g}' ./pkg/BOOLR.js
+sed -i '/typeof .* == '\''function'\'' ? .* : notDefined('\''.*'\'');/{s! : notDefined('\''!(); } : notDefined('\''!g}' ./pkg/BOOLR.js
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,37 @@
 #![allow(dead_code)]
-extern crate js_sys;
-extern crate wasm_bindgen;
-extern crate web_sys;
-
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::Element;
 use web_sys::HtmlElement;
 
-mod audio;
+mod utils;
+use crate::utils::set_panic_hook;
+
+/* mod audio;
 mod dialog;
 mod canvas;
 mod components;
 mod localstorage;
 mod saved_custom_components;
-mod toolbar;
+mod toolbar; */
 
 // from startup.js
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    set_panic_hook();
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    canvas::init()?;
+    hide();
+    let closure = Closure::wrap(Box::new(move || {
+        show();
+    }) as Box<dyn Fn()>);
+    window.set_timeout_with_callback_and_timeout_and_arguments_0(
+        closure.as_ref().unchecked_ref(),
+        1000,
+    )?;
+    closure.forget();
+    return Ok(());
+    //canvas::init()?;
     /*
     getLocalStorage(localStorage.pwsData);
     getCustomComponents();
@@ -67,4 +77,18 @@ pub fn main() -> Result<(), JsValue> {
 #[wasm_bindgen]
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = dialog)]
+    fn show();
+    #[wasm_bindgen(js_namespace = dialog)]
+    fn hide();
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(_: String);
 }
